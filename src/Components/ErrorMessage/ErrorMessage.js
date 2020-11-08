eec295b306b4064a44a1ccf8e3f070266cf68c4a
carl_meyrick <carlmeyrickcheng@gmail.com>
2020-11-08 19:01:10 +0800

    Finalize error message

--- src/Components/ErrorMessage/ErrorMessage.js
+++ src/Components/ErrorMessage/ErrorMessage.js
@@ -6,23 +6,29 @@ const ErrorMessage = (props) => {
 
     const generateErrorMessage = (data) => {
 
-        if ("errorType" in data) {
-            return (
-                <Aux>
-                    <div className={`row center-align ${classes.errorMessageHeader}`}>
-                        <i className="large material-icons prefix">error</i>
-                    </div>
-                    <div className={`row center-align`}>
-                        <p className={`flow-text ${classes.leadMessage}`}>Oh no! There's a problem with your search. It's either of the following:</p>
-                        <p className={`flow-text ${classes.reasons}`}>You have entered either the wrong session id or query hash.</p>
-                        <p className={`flow-text ${classes.reasons}`}>The Instragram username doesn't exists.</p>
-                        <p className={`flow-text ${classes.reasons}`}>Your Instagram account has been blocked.</p>
-                    </div>
-                </Aux>
+        return (
+            <Aux>
+                <div className={`row center-align ${classes.errorMessageHeader}`}>
+                    <i className="large material-icons prefix">error</i>
+                </div>
+                {
+                    ("errorType" in data) ? 
+                        <div className={`row center-align`}>
+                            <p className={`flow-text ${classes.leadMessage}`}>Oh no! There's a problem with your search. It's either of the following:</p>
+                            <p className={`flow-text ${classes.reasons}`}>You have entered either the wrong session id or query hash.</p>
+                            <p className={`flow-text ${classes.reasons}`}>The Instragram username doesn't exists.</p>
+                            <p className={`flow-text ${classes.reasons}`}>Your Instagram account has been blocked.</p>
+                        </div> : 
+                    (data.name === "Error") ?
+                        <div className={`row center-align`}>
+                            <p className={`flow-text ${classes.leadMessage}`}>{data.message}</p>
+                        </div> :
+                        <div className={`row center-align`}>
+                            <p className={`flow-text ${classes.leadMessage}`}>Unknown Error</p>
+                        </div>
+                }
+            </Aux>
             )
-        }
-
-
     }
 
     return (
--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -25,26 +25,11 @@ const Main = () => {
         activateLoading(true);
         disableSearchButton(true);
         axios(config).then(function (response) {
-            //console.log(response.data);
-            // {
-            //     "errorType": "TypeError",
-            //     "errorMessage": "Cannot read property 'user' of undefined",
-            //     "trace": [
-            //         "TypeError: Cannot read property 'user' of undefined",
-            //         "    at /var/task/index.js:35:38",
-            //         "    at Array.filter (<anonymous>)",
-            //         "    at /var/task/index.js:33:24",
-            //         "    at runMicrotasks (<anonymous>)",
-            //         "    at processTicksAndRejections (internal/process/task_queues.js:97:5)",
-            //         "    at async Runtime.exports.handler (/var/task/index.js:163:22)"
-            //     ]
-            // }
             setFetchResult(response.data);
             activateLoading(false);
             disableSearchButton(false);
         }).catch(function (error) {
             setFetchResult(error);
-            //{"message":"Network Error","name":"Error","stack":"Error: Network Error\n    at createError (http://localhost:3000/static/js/0.chunk.js:1040:15)\n    at XMLHttpRequest.handleError (http://localhost:3000/static/js/0.chunk.js:536:14)","config":{"url":"https://9tr1t1v7x1.execute-api.ap-southeast-1.amazonaws.com/Development/getinstagramaccounts1","method":"get","headers":{"Accept":"application/json, text/plain, */*"},"params":{"sessionId":"3663508345%3AWnVxoRWJ5fv0qz%3A15","query_hash":"d4d88dc1500312af6f937f7b804c68c3","username":"jollibee","max_followers":"1000000","min_followers":"200"},"transformRequest":[null],"transformResponse":[null],"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","maxContentLength":-1,"maxBodyLength":-1}}
             activateLoading(false);
             disableSearchButton(false);
         });
@@ -55,7 +40,7 @@ const Main = () => {
         if (isLoading) {
             return <Spinner />
         } else {
-            if ((Object.keys(results).length === 0 && results.constructor === Object) || results.name === "Error" || "errorType" in results) {
+            if (results.name === "Error" || "errorType" in results) {
                 return <ErrorMessage errorResults={results} />
             } else {
                 return <TableResults results={results} />
